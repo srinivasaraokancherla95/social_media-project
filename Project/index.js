c55e36d5d6d74925ef5d5496130d774e1ff9334a
Navyatha Kolluru <kollurunavyatha@gmail.com>
2023-12-12 00:15:48 +0530

    Movie Seat Slot Booking

--- Project/index.js
+++ Project/index.js
@@ -1,14 +1,43 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename= fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import express from 'express'
-
 import hbs from 'hbs'
-
 import bodyParser from 'body-parser'
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser'
+import mongoose from 'mongoose'
 
 import {readPosts, readUser, insertPost, insertUser, likeFun, shareFun} from './operation.js'
 
 const app = express()
+mongoose .connect("mongodb://localhost:27017/cinema", {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
+const screen1Model = mongoose.model('screen1',{
+    seatno:{type:Number},
+    status:{type:String}
+})
+
+const screen2Model = mongoose.model('screen2',{
+    seatno:{type:Number},
+    status:{type:String}
+})
+
+const screen3Model = mongoose.model('screen3',{
+    seatno:{type:Number},
+    status:{type:String}
+})
+
+const moviesModel = mongoose.model('movies',{
+    name: {type:String},
+    rate: {type:Number},
+    screenNo: {type:Number}
+})
 
 app.set('view engine', 'hbs')
 
@@ -17,10 +46,60 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+var screen1Res
+screen1Model.find()
+.then(function(output){
+  screen1Res = output
+})
+.catch(function(err){
+console.log(err)
+
+})
+
+var screen2Res
+screen2Model.find()
+.then(function(output){
+  screen2Res = output
+})
+.catch(function(err){
+console.log(err)
+
+})
+
+var screen3Res
+screen3Model.find()
+.then(function(output){
+  screen3Res = output
+})
+.catch(function(err){
+console.log(err)
+
+})
+
+var moviesRes
+moviesModel.find()
+.then(function(output){
+  moviesRes = output
+})
+.catch(function(err){
+console.log(err)
+
+})
+
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get (`/`, (req, res)=>{
     res.render("login")
+})
+
+app.get('/cinema', (req, res)=>{
+    res.render("cinema", {
+        movies:moviesRes,
+        screen1:screen1Res,
+        screen2:screen2Res,
+        screen3:screen3Res
+    })
 })
 
 app.post('/login', async(req, res)=>{
